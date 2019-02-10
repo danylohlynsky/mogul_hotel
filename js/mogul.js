3242acb308d474be5cd2d0120fff3bdea7345567
Danylo Hlynsky <danylohlynsky@gmail.com>
2019-02-10 23:27:14 +0000

    Slider

---
+++ js/mogul.js
@@ -0,0 +1,57 @@
+let slides = document.querySelectorAll('.slide-single');
+let slider = [];
+for (let i = 0; i < slides.length; i++) {
+    slider[i] = slides[i].src;
+    slides[i].remove();
+}
+let step = 0;
+let offset = 0;
+
+function draw() {
+    let img = document.createElement('img');
+    img.src = slider[step];
+    img.classList.add('slide-single');
+    img.style.left = offset * 720 + 'px';
+    document.querySelector('#slider').appendChild(img);
+    if (step + 1 === slider.length) {
+        step = 0;
+    }
+    else {
+        step++;
+    }
+    offset = 1;
+}
+
+function left() {
+    let slidesView = document.querySelectorAll('.slide-single');
+    let offsetView = 0;
+    for (let i = 0; i < slidesView.length; i++) {
+        slidesView[i].style.left = offsetView * 720 - 720 + 'px';
+        offsetView++;
+    }
+    setTimeout(function() {
+        left();
+        slidesView[0].remove();
+        draw();
+    }, 2500);
+}
+
+draw();
+draw();
+left();
+
+
+// autoSlider();
+// let left = 0;
+
+// function autoSlider() {
+//     setTimeout(function() {
+//         let strip = document.getElementById('strip');
+//         left -= 720;
+//         if (left === -2160) {
+//             left = 0;
+//         }
+//         strip.style.left = left + 'px';
+//         autoSlider();
+//     }, 2000);
+// }
